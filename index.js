5ca5529071008717ac5b76c1cc368249376e2216
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-26 21:23:04 +0530

    via upload

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");// npm i method-ovverride
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
-app.use(express.urlencoded({extended:true})); 
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method")); 
 
 main()
 .then(() =>{
@@ -19,20 +21,20 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
 
-let chat1 = new Chat({
-    from : "neha",
-    to : "rahul",
-    msg : "Send me your thought",
-    created_at : new Date(),
-})
-chat1.save().then((res)=>{ //UTC
-    console.log(res);
-})
+// let chat1 = new Chat({
+//     from : "neha",
+//     to : "rahul",
+//     msg : "Send me your thought",
+//     created_at : new Date(),
+// })
+// chat1.save().then((res)=>{ //UTC
+//     //console.log(res);
+// })
 
 // Index Route
 app.get("/chats", async(req,res) =>{
     let chats = await Chat.find();
-    console.log(chats);
+    // console.log(chats);
     res.render("index.ejs",{chats});
 })
 
@@ -51,13 +53,32 @@ app.post("/chats",(req,res) =>{
         created_at : new Date()
     });
     newChat.save()
-    .then((res) =>{
-         console.log("Chat was Saved")
+    .then(() =>{
+         console.log("Chat was Saved");
         })
     .catch((err) =>{
             console.log(err);
         });
     res.redirect("/chats");    
+});
+
+// Edit Route 
+app.get("/chats/:id/edit",async(req,res) =>{
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs",{chat} );
+});
+//Update Route 
+app.put("/chats/:id", async(req,res) =>{
+    let { id } = req.params;
+    let { msg:newMsg } = req.body;
+   
+    let updatedChat = await Chat.findByIdAndUpdate(
+        id ,
+        {msg :newMsg},
+        {runValidators:true , new :true});
+    //console.log(updatedChat);
+    res.redirect("/chats");
 });
 
 app.get("/", (req,res) =>{
