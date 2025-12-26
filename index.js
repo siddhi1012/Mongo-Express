30100dbea0f1aab8a1b272b8081b064deeb026a5
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-26 18:01:25 +0530

    via upload

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Chat = require("./models/chat.js");
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true})); 
 
 main()
 .then(() =>{
@@ -35,6 +36,29 @@ app.get("/chats", async(req,res) =>{
     res.render("index.ejs",{chats});
 })
 
+// New Route 
+app.get("/chats/new" , (req,res) =>{
+    res.render("new.ejs");
+
+});
+// Create Route
+app.post("/chats",(req,res) =>{
+    let { from, to, msg } = req.body;
+    let newChat = new Chat({
+        from : from ,
+        to : to,
+        msg : msg,
+        created_at : new Date()
+    });
+    newChat.save()
+    .then((res) =>{
+         console.log("Chat was Saved")
+        })
+    .catch((err) =>{
+            console.log(err);
+        });
+    res.redirect("/chats");    
+});
 
 app.get("/", (req,res) =>{
     res.send("Root is Working");
