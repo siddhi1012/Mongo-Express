9f111d90bb146143c0b3458e4e62b3e910bcf88f
Siddhi Gavhane <siddhigavhane03@gmail.com>
2026-01-02 22:36:04 +0530

    via upload

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
 const methodOverride = require("method-override");// npm i method-ovverride
+const ExpressError = require("./ExpressError.js");
+
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
@@ -61,6 +63,13 @@ app.post("/chats",(req,res) =>{
         });
     res.redirect("/chats");    
 });
+
+// New - Show Route
+app.get("/chats/:id",async(req,res,next) =>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.js",{chat});
+})
 
 // Edit Route 
 app.get("/chats/:id/edit",async(req,res) =>{
